aae32443b1fa11cfd70dbd7c70830caf233d1cbf
chenp <chenpeleg@gmail.com>
2022-10-31 20:30:16 +0200

    added vscode files

--- app/app.js
+++ app/app.js
@@ -6,10 +6,15 @@ const app = C();
 document.body.appendChild(app.elem);
 
 app.append(
-    C(`<h1>App</h1>`)
-        .styler({ width: '100vw', height: '100vh', 'background-color': 'aqua' })
-        .append(C(`<li> This is a js vanilla app</li>`)),
+    C(`<h1>App</h1>`).append(C(`<li> This is a js vanilla app</li>`)),
     C(`<li> This is a js vanilla app</li>`),
     C(`<li> This is a js vanilla app 1 </li>`),
     C(`<li> This is a js vanilla app 2</li>`)
-);
+).styler({
+    'box-sizing': 'border-box',
+    overflow: 'hidden',
+    padding: '40px',
+    width: '100%',
+    height: '100vh',
+    'background-color': 'aqua',
+});
